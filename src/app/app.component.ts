d5e3b542f98f36d0d2ac9699e2154adde783c4e6
toan nguyen <ngdtoan2k@gmail.com>
2024-05-07 08:59:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'my-app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from "./user-list/user-list.component";
+import { HttpClientModule } from '@angular/common/http';
+import { UserserviceService } from './userservice.service';
+import { FormsModule } from '@angular/forms';
+import { UserdetailComponent } from './userdetail/userdetail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+       
+    ],
+    providers: [UserserviceService],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        UserListComponent,
+        UserdetailComponent,
+        HttpClientModule,
+        FormsModule
+    ]
 })
 export class AppModule { }
